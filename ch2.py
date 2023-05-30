bac4f239e804b3a29acb5ddc5c26697a54f4b4c1
Matías <matiasgcl@protonmail.com>
2023-05-30 16:19:12 +0200

    new files - up to chap3 exercises and examples

---
+++ ch2.py
@@ -0,0 +1,65 @@
+# CH02.M  Frequency with which conf interval contains exact mean.
+#         Exact mean is known to equal 1.
+
+import numpy as np
+import numpy.random as random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+random.seed(100);             # set random number generator seed
+se = np.sqrt(np.exp(1));
+ 
+M=10**4;
+Ms = [10**2, 10**3, 10**4, 10**5, 10**6, 10**7];
+repeats = [10**2, 10**3, 10**4];
+length = len(repeats);
+m = np.zeros(length);
+s_mean = np.zeros(length);
+s_std = np.zeros(length);
+hitfreq = np.zeros(length);
+
+for i in range(length):
+  reps = repeats[i];
+  hits = 0;
+  for k in range(reps):
+      x = np.exp(random.normal(0,1,M))/se;   # samples
+      s_mean = np.mean(x);         # sample mean
+      s_std = np.std(x,ddof=1);           # sample standard deviation
+      ebar = 1.96*s_std/np.sqrt(M); 
+      clower = s_mean-ebar;     # left endpoint of C.I.
+      cupper = s_mean+ebar;     # right endpoint of C.I.
+      if ((s_mean-ebar<1) and (s_mean+ebar>1)):
+        hits = hits+1;          # increment if successful
+  hitfreq[i] = hits/reps;  # required frequency 
+
+print('frequencies = '+str(hitfreq))
+
+# Exercise 1 - Intervals and sample means, varying M.
+plt.figure(figsize = (10, 10))
+for j in range(len(Ms)):
+    x = np.exp(random.normal(0,1,Ms[j]))/se;   # samples
+    s_mean = np.mean(x);         # sample mean
+    s_std = np.std(x,ddof=1);           # sample standard deviation
+    ebar = 1.96*s_std/np.sqrt(Ms[j]); 
+    clower = s_mean-ebar;     # left endpoint of C.I.
+    cupper = s_mean+ebar;     # right endpoint of C.I.
+    plt.vlines(x = Ms[j], ymin = clower, ymax = cupper,
+           colors = 'blue',
+           label = 'vline_multiple - full height')
+    plt.plot(Ms[j], s_mean, marker="x")           
+    plt.xlabel('M: Number of samples')
+    plt.xscale('log')
+    plt.ylabel('Sample mean & Confidence intervals')
+    plt.axhline(y=1, color='r', linestyle='dotted', linewidth=0.5)
+
+plt.show()
+
+# Exercise 2 - CLT illustration with Z_i = sin(2pi Y_i), Y_i ~ Unif(0,1).
+x = np.zeros(10**4);
+for j in range(10**4):
+    u = np.sin(2*np.pi*random.uniform(0,1,10**4));   # samples
+    x[j] = u.sum();
+
+v = x/(np.sqrt(10**4)*1/4) # CLT affirms (Sum Z_i - ME(Z))/(sqrt(M)V(Z)) approaches a rv N ~ N(0,1) 
+sns.displot(v,kde=True); # Lets verify it!
+plt.show();
---
+++ ch3.py
@@ -0,0 +1,111 @@
+import numpy as np
+import numpy.random as random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+# brute-classical for
+L = 101;
+T = 1;
+dt = T/L;
+xvals = np.linspace(0,T,L);
+W = np.zeros(L);
+xi = np.random.normal(0,1,L);
+start_time = time.time();
+for i in range(L-1):
+    W[i+1] = W[i] + np.sqrt(dt)*xi[i];
+elapsed = time.time()-start_time;
+print(elapsed);
+plt.plot(xvals,W);
+plt.title('MB W(t)');
+plt.xlabel('t');
+plt.ylabel('W(t)');
+plt.grid(True);
+plt.show();
+
+# vectorized(ish)
+dB = np.zeros(L);
+B = np.zeros(L);
+start_time = time.time();
+dB = np.sqrt(dt)*xi;
+dB[0] = 0; # Do not forget!
+B = np.cumsum(dB);
+elapsed2 = time.time()-start_time;
+print(elapsed2);
+ratio = elapsed2/elapsed*100;
+print('vectorized method runs in '+str(ratio)+'% of time elapsed using a for loop');
+plt.plot(xvals,B);
+plt.title('MB B(t)');
+plt.xlabel('t');
+plt.ylabel('B(t)');
+plt.grid(True);
+plt.show();
+
+def buildMB(L,T):
+    # vectorized(ish)
+    dB = np.zeros(L);
+    B = np.zeros(L);
+    dB = np.sqrt(dt)*np.random.normal(0,1,L);
+    dB[0] = 0; # Do not forget!
+    B = np.cumsum(dB);
+    return(B);
+
+# exercise 1: Refining the BM (vectorized).
+
+xvalsref = np.linspace(0,T,2*L-1);
+aux = 0.5*(W+np.roll(W,-1))+np.sqrt(dt)*np.random.normal(0,1,L);
+Bref = np.zeros(2*L-1);
+Bref[::2] = B;
+Bref[1::2] = aux[0:L-1];
+plt.plot(xvalsref,Bref, color='red',linestyle='dashed', marker='x');
+plt.plot(xvals,B, color='green', marker='o');
+plt.title('B_refined(t) (red, dashed, x) and B(t) (green, o)');
+plt.xlabel('t');
+plt.ylabel('B(t) and B_ref(t)');
+plt.grid(True);
+plt.show();
+
+# exercise 2: Exploring the scaling.
+c = 1/10;
+Tc = T*(c*c);
+dtc = Tc/L;
+dBc = np.zeros(L);
+Bc = np.zeros(L);
+dBc = np.sqrt(dtc)*np.random.normal(0,1,L);
+dBc[0] = 0; # Do not forget!
+Bc = 1/c*np.cumsum(dBc);
+xcvals = np.linspace(0,Tc,L);
+plt.plot(xcvals,Bc);
+plt.title('V(t)=1/c*W(c^2*t)');
+plt.xlabel('t');
+plt.ylabel('V(t)');
+plt.grid(True);
+plt.show();
+
+# example: u(t,W(t)) = exp(t+1/4*W(t))
+# plot 5 different trajectories (t,u(t,W(t))
+# compute M different trajectories to get a sample mean (MonteCarlo!) - dashed blue
+# and compares with theoretical mean (exp(33/32*t)) - dashed black
+# for M ~ 100 it is already quite good approximation.
+
+u = np.zeros([5,L])
+samp_mean = np.zeros(L);
+M=100
+for i in range(M):
+    if(i<5):
+        u[:][i] = np.exp(xvals + 1/4*buildMB(L,T));
+        plt.plot(xvals,u[:][i]);
+        aux = u[:][i];
+    else:
+        aux = np.exp(xvals + 1/4*buildMB(L,T));
+    samp_mean += aux
+samp_mean *= 1/M;
+exact_mean = np.exp(33/32*xvals);
+plt.plot(xvals,samp_mean,linestyle='dashed',color='blue');
+plt.plot(xvals,exact_mean,linestyle='dashed',color='black');
+plt.title('u(t,W(t)) = exp(t+1/4*W(t))');
+plt.xlabel('t');
+plt.ylabel('u(t,W(t))');
+plt.grid(True);
+plt.show();
+
